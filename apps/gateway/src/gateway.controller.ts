1f560fd01437e6993e383fac6934e7522fe2a56d
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-12-23 21:44:32 +0330

    start change in gateway app

---
+++ apps/gateway/src/gateway.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { GatewayService } from './gateway.service';
+
+@Controller()
+export class GatewayController {
+  constructor(private readonly gatewayService: GatewayService) {}
+
+  @Get()
+  getHello(): string {
+    return this.gatewayService.getHello();
+  }
+
+  @Get()
+  health() {
+    return { status: 'ok', time: new Date().toLocaleDateString(), service: 'Gateway' };
+  }
+}
